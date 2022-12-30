7edd8f6ae60d5a43d79e033aef1f693b63f4ee16
ketlasdev <hsn.ketlas@gmail.com>
2022-12-30 23:51:42 +0100

    init immatriculation query service

---
+++ immatriculation-query-service/src/main/java/me/ketlas/immatriculationqueryservice/ImmatriculationQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package me.ketlas.immatriculationqueryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmatriculationQueryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmatriculationQueryServiceApplication.class, args);
+    }
+
+}
---
+++ immatriculation-query-service/src/test/java/me/ketlas/immatriculationqueryservice/ImmatriculationQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ketlas.immatriculationqueryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmatriculationQueryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
